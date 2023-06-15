784042563e37f3e22575ec5966fac242ba0955c2
Shivakant6387 <shivakant5875@gmail.com>
2023-06-15 11:36:54 +0530

    application properties data base

---
+++ src/main/java/com/example/springbootproblemsolvingapplication/SpringBootProblemSolvingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootproblemsolvingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProblemSolvingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProblemSolvingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootproblemsolvingapplication/SpringBootProblemSolvingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootproblemsolvingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProblemSolvingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
